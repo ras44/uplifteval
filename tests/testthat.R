3f571324a99d62fc0f8f2226902c76c6c1561838
Roland Stevenson <roland@rmg-services.com>
2019-04-22 14:34:29 +0000

    added tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(uplifteval)
+
+test_check("uplifteval")
---
+++ tests/testthat/test_placeholder.R
@@ -0,0 +1,8 @@
+context("placeholder tests")
+library(whisker)
+
+test_that(" 1==1 ", {
+
+  expect_true(1==1)
+
+})
